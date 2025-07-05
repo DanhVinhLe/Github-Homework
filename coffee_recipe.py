992d903f1c5dce10be48a46d390af9e52ebd4de4
danhvinhle-zenai <danhvinhle.zenai@gmail.com>
2025-07-06 00:29:44 +0700

    Fix coffee typo

--- coffee_recipe.py
+++ coffee_recipe.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     print("1. Boil water")
     print("2. Add coffee grounds")
     print("3. Pour water over coffee")
-    print("4. Let it steep")
-    print("5. Enjoy your coffee!")
+    print("4. Add sugar and milk (optional)")
+    print("5. Let it steep")
+    print("6. Enjoy your coffee!")
