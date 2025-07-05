d26f97eb61750bdb81a76f1c0a83dd046db9eb35
danhvinhle-zenai <danhvinhle.zenai@gmail.com>
2025-07-06 00:28:02 +0700

    Add coffee recipe

---
+++ coffee_recipe.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("Coffee Recipe:")
+    print("1. Boil water")
+    print("2. Add coffee grounds")
+    print("3. Pour water over coffee")
+    print("4. Let it steep")
+    print("5. Enjoy your coffee!")
